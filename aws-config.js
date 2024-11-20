ac18473d781efbb598d6e1847732aae74d46596d
New-mean <dmb08075@naver.com>
2024-11-20 15:18:07 +0900

    test

--- aws-config.js
+++ aws-config.js
@@ -6,7 +6,7 @@ const s3 = new S3Client({
     credentials: {
         accessKeyId: process.env.AWS_ACCESS_KEY_ID,
         secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-    },
+    },  
 });
 
 module.exports = s3;
